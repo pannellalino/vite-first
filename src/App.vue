3bbf6642beafc582e8b55c50c12559a6f6bbbd68
pannellalino <lista>
2022-11-14 16:30:02 +0100

    first

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+  import Title from './components/TitleComponent.vue';
+
+  export default {
+    name: 'First',
+    components: {
+      Title
+    }
+  }
+</script>
+
+
+<template>
+  <Title/>
+
+</template>
+
+
+<style>
+
+</style>
---
+++ src/components/TitleComponent.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: 'Title'
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>La mia prima app con Vite!</h1>
+  </div>
+</template>
+
+
+<style scope>
+  h1{
+    color: lime;
+  }
+</style>
